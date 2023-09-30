f683869845fb116099e02dd63fdeabc8df4a5cda
Gregory Caesar <gregmcaesar@gmail.com>
2023-09-29 20:29:39 -0500

    Initial commit

--- server/server.js
+++ server/server.js
@@ -1,13 +1,11 @@
 import express from 'express'
-
 import './config/dotenv.js'
-
+import cors from 'cors'
 import giftsRouter from './routes/gifts.js'
 
 const app = express()
 
-app.use('/public', express.static('./public'))
-app.use('/scripts', express.static('./public/scripts'))
+app.use(cors())
 
 app.use('/gifts', giftsRouter)
 
@@ -16,7 +14,8 @@ app.get('/', (req, res) => {
 })
 
 const PORT = process.env.PORT || 3001
-    
+
 app.listen(PORT, () => {
   console.log(`🚀 Server listening on http://localhost:${PORT}`)
 })
+
